d3fb07cebb72027b1fa391a2847d18b9b903bee1
FlutLab.io <>
2021-03-02 12:24:33 +0000

    Autosave

--- lib/screens/screen_2.dart
+++ lib/screens/screen_2.dart
@@ -5,7 +5,7 @@ class Screen2 extends StatelessWidget {
   const Screen2({Key key}) : super(key: key);
 
   static const routeName = '/screen_2';
-
-  @override
-  Widget build(BuildContext context) => ButtonWidget();
+  ButtonWidget();
+  /*@override
+  Widget build(BuildContext context) => ButtonWidget();*/
 }
