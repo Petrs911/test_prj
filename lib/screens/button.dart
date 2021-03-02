65cbd6f1e1130250efe9e3494c5efa2bb632a021
FlutLab.io <>
2021-03-02 17:29:41 +0000

    Autosave

--- lib/screens/button.dart
+++ lib/screens/button.dart
@@ -10,7 +10,7 @@ class ButtonWidget extends StatelessWidget {
       child: Align(
         alignment: Alignment.topRight,
         child: IconButton(
-          iconSize: 70,
+          iconSize: 50,
           icon: Icon(CupertinoIcons.clear),
           color: Colors.black,
           onPressed: () => Navigator.pop(context),
