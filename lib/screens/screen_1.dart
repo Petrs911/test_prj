1a19c8efedfb7df502467554a4cfcb9fa2360ff6
FlutLab.io <>
2021-03-02 14:11:59 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -19,7 +19,7 @@ class Screen1 extends StatelessWidget {
             child: Text("TEST BUTTON"),
             onPressed: () {},
             style: ElevatedButton.styleFrom(
-              minimumSize: Size(40.0, 60.0),
+              minimumSize: Size(60.0, 60.0),
               primary: Colors.pink,
               onPrimary: Colors.white,
               shape: RoundedRectangleBorder(
