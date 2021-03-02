78dbf5920ff7dadb6c1447f7a99103b6415fee3b
FlutLab.io <>
2021-03-02 17:06:41 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -33,7 +33,7 @@ class Screen1 extends StatelessWidget {
           child: Container(
             //margin: EdgeInsets.all(15.0),
             padding: EdgeInsets.all(3.0),
-            //height: 80.0,
+            height: 581.0,
             decoration: BoxDecoration(
               border: Border.all(
                 color: Colors.blueAccent,
