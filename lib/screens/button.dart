658695e720969cff291a5d52aa7523c7fe15149f
FlutLab.io <>
2021-03-02 17:19:23 +0000

    Autosave

--- lib/screens/button.dart
+++ lib/screens/button.dart
@@ -8,12 +8,12 @@ class ButtonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Padding(
-        padding: EdgeInsets.all(2.0),
+        padding: EdgeInsets.all(20.0),
         child: Align(
           alignment: Alignment.topRight,
           child: IconButton(
             iconSize: 30,
-            icon: Icon(CupertinoIcons.clear, size: 50.0),
+            icon: Icon(CupertinoIcons.clear, size: 80.0),
             color: Colors.black,
             onPressed: () => Navigator.pop(context),
           ),
