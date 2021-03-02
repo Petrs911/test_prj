bc67731938a4f225652f391f16700620028e8839
FlutLab.io <>
2021-03-02 01:01:58 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -17,7 +17,7 @@ class Screen1 extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(20.0),
           child: IconButton(
-            icon: Icon(Icons.clear, size: 30.0),
+            icon: Icon(Icons.clear, size: 40.0),
             onPressed: () => Navigator.pop(context),
           ),
         ), 
