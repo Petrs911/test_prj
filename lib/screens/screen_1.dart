f84c270be5ceb3c183cd033d3348aafed754ead1
FlutLab.io <>
2021-03-02 13:26:20 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -14,6 +14,7 @@ class Screen1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
+        SizedBox(width: 10),
         child: RaisedButton(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(18.0),
