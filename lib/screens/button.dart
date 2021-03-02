e659d0a1e5cd0047a802ff17b0e24bdeaff70fb7
FlutLab.io <>
2021-03-02 12:34:28 +0000

    Autosave

--- lib/screens/button.dart
+++ lib/screens/button.dart
@@ -8,7 +8,7 @@ class ButtonWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       child: Padding(
-        padding: EdgeInsets.all(30.0),
+        padding: EdgeInsets.all(20.0),
         child: Align(
           alignment: Alignment.topRight,
           child: IconButton(
