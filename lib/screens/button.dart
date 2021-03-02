80b136b1c3ac6d0f0a6fe93b36ff9e21411abf78
FlutLab.io <>
2021-03-02 20:30:36 +0000

    Autosave

--- lib/screens/button.dart
+++ lib/screens/button.dart
@@ -11,7 +11,7 @@ class ButtonWidget extends StatelessWidget {
       child: Column(
         children: <Widget>[
         icon: Icon(CupertinoIcons.clear),
-        ]),
+      ],),
       ),
     );
   }
