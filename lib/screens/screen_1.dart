e4bcda6b713dda6063e4d57eb52435cf3328b6c5
FlutLab.io <>
2021-03-03 16:16:06 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -17,7 +17,7 @@ class Screen1 extends StatelessWidget {
             child: Column(
               children: <Widget> [
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget> [
                     Container(),
                     Text('Title', style: TextStyle(
