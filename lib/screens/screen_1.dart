91b82ad416cb2f4af1a00d34cfa597c951f7bda2
FlutLab.io <>
2021-03-03 15:28:01 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -53,7 +53,9 @@ class Screen1 extends StatelessWidget {
                   decoration: BoxDecoration(
                     color: Color(0xFFE5E5E5),
                   ),
-                  child: Expanded(
+                  child: CupertinoScrollbar(
+                    isAlwaysShown: true,
+                    controller: ScrollController(),
                     child: SingleChildScrollView(
                     scrollDirection: Axis.vertical,
                     child: Text("A box in which a single widget can be scrolled. This widget is useful when you have a single box that will normally be entirely visible, for example a clock face in a time picker, but you need to make sure it can be scrolled if the container gets too small in one axis (the scroll direction). It is also useful if you need to shrink-wrap in both axes (the main scrolling direction as well as the cross axis), as one might see in a dialog or pop-up menu. In that case, you might pair the [SingleChildScrollView] with a [ListBody] child. When you have a list of children and do not require cross-axis shrink-wrapping",
