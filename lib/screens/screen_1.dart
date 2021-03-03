d837cdbd67f9d0087f4c108175f8e4bedcb6b8e1
FlutLab.io <>
2021-03-03 14:42:35 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -32,7 +32,7 @@ class Screen1 extends StatelessWidget {
      return  Center(
           child: Container(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               //mainAxisSize: MainAxisSize.min,
               children: <Widget> [
                 Align(
