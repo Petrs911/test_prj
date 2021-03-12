f8cf4d7cfd8553cf6ee2b8310a17a14b0b26c6c6
FlutLab.io <>
2021-03-12 17:59:25 +0000

    Autosave

--- lib/screens/screen_1.dart
+++ lib/screens/screen_1.dart
@@ -19,6 +19,7 @@ class Screen1 extends StatelessWidget {
         decoration: BoxDecoration(
           color: Colors.green,
           borderRadius: BorderRadius.circular(12),
+        )
         child: Column(
           children: <Widget> [
             Row(
@@ -34,7 +35,6 @@ class Screen1 extends StatelessWidget {
               ],
             ),
           ],
-        ),
         ),
       ),
     );
