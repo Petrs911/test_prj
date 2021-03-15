5732a8d2a61d570753abd8b98c4f1b2506fa9508
FlutLab.io <>
2021-03-15 16:18:55 +0000

    Autosave

--- lib/screens/build_lists/horizontal_list_view_builder.dart
+++ lib/screens/build_lists/horizontal_list_view_builder.dart
@@ -8,27 +8,25 @@ class HorizontalListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var item = context.watch<HorizontalItemsProvider>();
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Padding(
-          padding: EdgeInsets.only(left: 8.0),
-          child: Text(
-            'Section 1 (${item.itemsCount})',
-            style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+      Padding(
+        padding: EdgeInsets.only(left: 8.0),
+        child: Text(
+          'Section 1 (${item.itemsCount})',
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+        ),
+      ),
+      Expanded(
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: item.itemsCount - 1,
+          itemBuilder: (context, index) => TextWidget(
+            text: item.itemAt(index),
+            height: 150.0,
+            weight: 330.0,
           ),
         ),
-        Expanded(
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: item.itemsCount - 1,
-            itemBuilder: (context, index) => TextWidget(
-              text: item.itemAt(index),
-              height: 150.0,
-              weight: 330.0,
-            ),
-          ),
-        ),
-      ]);
+      ),
+    ]);
   }
 }
